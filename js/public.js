6ea1885e68a25c02cd77614145dd43f11dad9a4c
caoyingming <1046659469@qq.com>
2017-03-10 09:45:57 +0800

    app

--- js/public.js
+++ js/public.js
@@ -1,6 +1,6 @@
 var winW=document.documentElement.clientWidth;
-var desW=1200;
-var scale=1200/100;
+var desW=750;
+var scale=750/100;
 document.documentElement.style.fontSize=winW/scale+"px";
 window.onresize=function(){
     var winW=document.documentElement.clientWidth;
